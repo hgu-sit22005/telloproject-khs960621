278ac2d35085754e00285ff5fd70ac9734a3bc63
khs960621 <khs960621@naver.com>
2019-11-24 23:20:57 +0900

    hw2 solved

--- bin/tellopro.py
+++ bin/tellopro.py
@@ -7,8 +7,11 @@ import TelloPro
 tello = Tello()
 
 command_lst = []
-command_lst.append(TelloPro.get_instance('takeoff'))
-command_lst.append(TelloPro.get_instance('land'))
+command_lst.append(TelloPro.get_instance('takeoff', -1))
+for i in range(4):
+	command_lst.append(TelloPro.get_instance('flip', i))
+command_lst.append(TelloPro.get_instance('land', -1))
+
 
 for command in command_lst:
 	tello.send_command_instance(command)
--- src/python_interface.cpp
+++ src/python_interface.cpp
@@ -3,8 +3,17 @@
 #include "TelloPro.h"
 #include "takeoff.h"
 #include "land.h"
+#include "up.h"
+#include "down.h"
+#include "left.h"
+#include "right.h"
+#include "forward.h"
+#include "back.h"
+#include "cw.h"
+#include "ccw.h"
+#include "flip.h"
 
-TelloPro* get_instance(boost::python::str _inst)
+TelloPro* get_instance(boost::python::str _inst, int _value)
 {
 	std::string instance = boost::python::extract<std::string>(_inst);
 
@@ -12,6 +21,24 @@ TelloPro* get_instance(boost::python::str _inst)
 	   return new Takeoff;
 	else if(instance == "land")
 		return new Land;
+	else if(instance == "up") // add
+		return new Up(_value); // add
+	else if(instance == "down") // add
+		return new Down(_value); // add
+	else if(instance == "left") // add
+		return new Left(_value); // add
+	else if(instance == "right") // add
+		return new Right(_value); // add
+	else if(instance == "forward") // add
+		return new Forward(_value); // add
+	else if(instance == "back") // add
+		return new Back(_value); // add
+	else if(instance == "cw") // add
+		return new Cw(_value); // add
+	else if(instance == "ccw") // add
+		return new Ccw(_value); // add
+	else if(instance == "flip") // add
+		return new Flip(_value); // add
 	else
 		return nullptr;
 }
